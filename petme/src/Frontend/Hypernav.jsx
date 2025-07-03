52afdc1bff5c190dfa81f4d6bbde5c1d89940df7
Tasos Anastasiadis <tasosanas2002@gmail.com>
2025-07-03 21:57:03 +0300

    General Changes

--- petme/src/Frontend/Hypernav.jsx
+++ petme/src/Frontend/Hypernav.jsx
@@ -8,7 +8,7 @@ const Hypernav = () => {
       <div className="center">ΚΛΕΑΝΘΟΥΣ 41, ΚΑΤΩ ΤΟΥΜΠΑ</div>
       <div className="right">
         <a
-          href="https://www.facebook.com/petshoppetme/" // Replace with actual page
+          href="https://www.facebook.com/petshoppetme/"
           target="_blank"
           rel="noopener noreferrer"
           className="facebook-link"
@@ -19,6 +19,20 @@ const Hypernav = () => {
             className="facebook-icon"
           />
           Facebook
+        </a>
+
+        <a href="#" className="account-link" aria-label="Ο λογαριασμός μου">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="account-icon"
+            viewBox="0 0 24 24"
+            width="16"
+            height="16"
+            fill="#333"
+          >
+            <path d="M12 2a5 5 0 110 10 5 5 0 010-10zm0 12c-4.42 0-8 2.24-8 5v1h16v-1c0-2.76-3.58-5-8-5z" />
+          </svg>
+          Ο λογαριασμός μου
         </a>
       </div>
     </div>
